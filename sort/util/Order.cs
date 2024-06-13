f1766cdbb3067f71211d1f55688e498de7381e37
Karoline Matilde Kruse <karolinematildekruse@macbook-air-tilhrende-karoline.home>
2024-06-13 19:17:09 +0200

    added bubblesort and save to csv functionality

---
+++ sort/util/Order.cs
@@ -0,0 +1,55 @@
+namespace Sort;
+/// <summary>
+/// Class <c>Util</c>
+/// </summary>
+/// <param name="item">Utility class - singleton pattern</param>
+public static class Util{
+    public static void BubbleSort(List<int> numbers)
+        {
+        int n = numbers.Count;
+        bool swapped;
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            swapped = false;
+
+            // sort unsorted 'portion': n - i - 1
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                // curr > next
+                if (numbers[j] > numbers[j + 1])
+                {
+                    // swap current and next
+                    int temp = numbers[j];
+                    numbers[j] = numbers[j + 1];
+                    numbers[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            // no sorting needed - early exit
+            if (!swapped)
+                break;
+        }
+    }
+
+    public static void QuickSort() {
+        //TODO: Implement quicksort
+
+    }
+    public static void SaveToCSV(NumberList numberList) {
+            //TODO: add to database instead and support other formats
+            
+            var csvFilePath = "data/sorted_numbers.csv";
+            //check if file exists and append
+            using (var writer = new StreamWriter(csvFilePath, append: true))
+            
+            {
+                writer.WriteLine(string.Join(",", numberList.Numbers));
+            }
+    }
+
+
+
+
+}
