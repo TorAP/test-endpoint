b6a71e23d3da52abfd28e2b9de6261e8d70577cd
Karoline Matilde Kruse <karolinematildekruse@macbook-air-tilhrende-karoline.home>
2024-06-13 19:35:51 +0200

    added GET endpoint

--- sort/obj/Debug/net8.0/sort.AssemblyInfo.cs
+++ sort/obj/Debug/net8.0/sort.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sort")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9df80ff1864afd7908a067887abcc508109d6b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1766cdbb3067f71211d1f55688e498de7381e37")]
 [assembly: System.Reflection.AssemblyProductAttribute("sort")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sort")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
